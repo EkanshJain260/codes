56429a0bdd494c2ab06a9d6d38eceb67afd9d7be
EkanshJain260 <121757663+EkanshJain260@users.noreply.github.com>
2023-01-01 22:37:29 +0530

    Add files via upload

---
+++ 03Class_PS_DS/01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n=7;
+    int start=0;
+    int end=n;
+    for (int i=0;i<n;i++){
+        while(n<4*n){
+
+        for (int j=0;j<n;j++){
+        if (start==i ||end==j){
+            printf("%d",n);
+        }
+        }
+        }
+    }
+   
+}
---
+++ 03Class_PS_DS/ps01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n,i,j,rows,k=0;
+ printf("Enter the number of rows\n");
+ scanf("%d",& rows);
+ n=(rows+1)/2;
+ for(i=1;i<=rows;i++)
+ if(rows%2==0){
+    if(i<=n)
+    k++;
+    if(i>n+1)
+    k--;
+    for(j=1;j<=rows;j++){
+   if(j>=n+1-k&&j<=s
+    }
+ }
+
+    
+}
